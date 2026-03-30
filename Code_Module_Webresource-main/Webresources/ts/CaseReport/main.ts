b51750b9a6f0176cbe47bacda2957a5db958c26b
Jorge Ubaldo <1231274@isep.ipp.pt>
2026-03-30 23:07:36 +0100

    User Story 01 - Case Report Warning Notification.

---
+++ Code_Module_Webresource-main/Webresources/ts/CaseReport/main.ts
@@ -0,0 +1,39 @@
+namespace CaseReport.Main {
+
+    const COMPLEXITY = 'vtd_complexity';
+
+    const COMPLEXITY_TYPES = {
+        BASIC: 953180000,
+        MODERATED: 953180001,
+        ADVANCED: 953180002,
+        EXTREME: 953180003,
+    };
+
+
+    export async function onLoad(executionContext: Xrm.Events.EventContext) {
+        const formContext = executionContext.getFormContext();
+
+        showCaseReportWarningNotification(executionContext);
+    }
+
+    function showCaseReportWarningNotification(executionContext: Xrm.Events.EventContext) {
+        const formContext = executionContext.getFormContext();
+        const complexityAttr = formContext.getAttribute(COMPLEXITY);
+        if (!complexityAttr) {
+            console.log("Atributo não encontrado! Nome usado: " + COMPLEXITY);
+            return;
+        }
+        const complexityStatus = complexityAttr.getValue();
+        formContext.ui.clearFormNotification('caseReportWarningNotification');
+
+        if (complexityStatus === COMPLEXITY_TYPES.BASIC || complexityStatus === COMPLEXITY_TYPES.MODERATED) {
+           return;
+        }else{
+             formContext.ui.setFormNotification(
+            'High Complexity Report: This case requires a detailed quality standards review.',
+            'WARNING',
+            'caseReportWarningNotification',
+            );
+        }
+    }
+}
